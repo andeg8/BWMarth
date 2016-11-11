58a48ec8693f74225858a66fe0d87a07a69540c7
andeg8 <andrew@degenova.net>
2016-11-11 11:41:17 -0500

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,20 @@
+function setup() {
+createCanvas (windowWidth,windowHeight,WEBGL);
+  
+}
+function draw() {
+ellipsoid(99,99,99,99);
+rotateX(frameCount*0.10);
+rotateY(frameCount*0.01);
+box(100,100,100,100);
+translate( 200 , 0 , 0);
+ellipsoid(40,40,40,40);
+translate( -400 , 0 , 0);
+ellipsoid(40,40,40,40);
+rotateX(frameCount*0.10);
+rotateY(frameCount*0.0);
+translate(200,200,0);
+ellipsoid(40,40,40,40);
+translate( 0,-400,0);
+ellipsoid(40,40,40,40);
+}
